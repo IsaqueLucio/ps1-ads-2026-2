ce1cdd39d7c492d7fc01340dd9cff810ef7c243b
isaque.lucio <isaque.o.lucio@gmail.com>
2026-08-17 23:47:22 -0300

    Update .gitignore to include environment and text files, modify HelloController for formatting, update application.properties for datasource configuration, and add SQL migration for customers table.

--- api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
+++ api/src/main/java/br/edu/fatecfranca/api/controllers/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
    @GetMapping("/")
    public String hello() {
        return "API em funcionamento!";
